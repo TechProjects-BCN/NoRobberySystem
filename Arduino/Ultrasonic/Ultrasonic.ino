7329bcb14f0d049b1dee97a87886cc3af36c0df7
rruizsape <128026036+rruizsape@users.noreply.github.com>
2024-01-31 09:54:26 +0100

    Codi boto

--- Arduino/Ultrasonic/Ultrasonic.ino
+++ Arduino/Ultrasonic/Ultrasonic.ino
@@ -6,9 +6,9 @@
 //Pins
 const int trigPin = 9;
 const int echoPin = 10;
+const int buttonPin = 4;    
 
 //No entenc
-
 Servo servo1;
 
 //Variables
@@ -16,36 +16,52 @@ long duration;
 int distance;
 int angle_inicial = 10;
 int angle_final = 180;
+int buttonState = 0;    
 
-void setup() {
+
+void setup() {  // S'executa només quan sinicia el programa
+
   pinMode(trigPin, OUTPUT); // TrigPin en Ouput (Pin 9)
   pinMode(echoPin, INPUT); // EchoPin en Input (Pin 10)
 
   servo1.attach(8);  // Servo al Pin 8
   servo1.write(angle_inicial);  // Servo al angle de tancar la comporta
 
+  pinMode(buttonPin, INPUT); // Boto en Input (Pin 4)
+
   Serial.begin(9600); // Iniciar Serial (Consola)
+
 }
-void loop() {
+void loop() {  // S'executa sempre en bucle (Loop principal)
+
+  buttonState = digitalRead(buttonPin); // Llegeix si el boto esta en off o on (High or Low)
+
+  if (buttonState == HIGH) { // Si el boto està en High (On) entra al if (S'activa el sistema)
+
+
   // Pin 9 a Low
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  // Pin 9 a High durant 10 ms
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  // Llegeix el Pin 10, en mc
-  duration = pulseIn(echoPin, HIGH);
-  // Calcula la distancia rebuda del Pin 10
-  distance = duration * 0.034 / 2;
-  // Escriu la distancia a la consola
-  Serial.print("Distance: ");
-  Serial.println(distance);
-  if (distance < 50){ // Si la distancia és menor a 50 entra al if
-    Serial.println("Yes");
-    servo1.write(angle_final); // Obra comporta
-    delay(8000); // Temps de delay abans de tancar la comporta (temps en ms)
-    servo1.write(angle_inicial); // Tanca la comporta
-    delay(1000); // Temps abans de tornar a començar el codi (temps en ms)
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+    // Pin 9 a High durant 10 ms
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+    // Llegeix el Pin 10, en mc
+    duration = pulseIn(echoPin, HIGH);
+    // Calcula la distancia rebuda del Pin 10
+    distance = duration * 0.034 / 2;
+    // Escriu la distancia a la consola
+    Serial.print("Distance: ");
+    Serial.println(distance);
+    delay(500);
+
+    if (distance < 50){ // Si la distancia és menor a 50 entra al if
+      Serial.println("Yes");
+      servo1.write(angle_final); // Obra comporta
+      delay(8000); // Temps de delay abans de tancar la comporta (temps en ms)
+      servo1.write(angle_inicial); // Tanca la comporta
+      delay(1000); // Temps abans de tornar a començar el codi (temps en ms)
+    }
+    delay(50)
   }
 } 
